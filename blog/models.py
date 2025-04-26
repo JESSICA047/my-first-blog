1ce249d4bf71c48722501237d5f7923c1f3d177f
JESSICA047 <jadzoyi20@sbbs.uhas.edu.gh>
2025-04-26 17:15:56 +0000

    models.py changed

--- blog/models.py
+++ blog/models.py
@@ -10,11 +10,11 @@ on_delete=models.CASCADE)
     text = models.TextField() 
     created_date = models.DateTimeField(default=timezone.now) 
     published_date = models.DateTimeField(blank=True, 
-null=True) 
+null=True)
  
     def publish(self): 
         self.published_date = timezone.now() 
-        self.save() 
+        self.save()
  
     def __str__(self): 
         return self.title 
